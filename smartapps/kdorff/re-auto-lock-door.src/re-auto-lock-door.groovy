621ff05cfd78a6ce60f4270d1e29138de4ae604e
Kevin Dorff <kevin.dorff@rackspace.com>
2019-02-05 11:58:37 -0600

    Improved structure and groovy-doc

--- smartapps/kdorff/re-auto-lock-door.src/re-auto-lock-door.groovy
+++ smartapps/kdorff/re-auto-lock-door.src/re-auto-lock-door.groovy
@@ -1,7 +1,7 @@
 definition(
     name: "Re-Auto Lock Door",
     namespace: "kdorff",
-    author: "kdorff based from work from Arnaud",
+    author: "kdorff",
     description: "Automatically locks the specified door N after closing or unlocking",
     category: "Safety & Security",
     iconUrl: "http://chittagongit.com/images/door-lock-icon/door-lock-icon-5.jpg",
@@ -9,7 +9,16 @@ definition(
     pausable: true
 )
 
-preferences{
+
+/**
+ * This code is based on "Enhanced Auto Lock Door" from Arnaud
+ * https://github.com/SmartThingsCommunity/SmartThingsPublic/tree/master/smartapps/lock-auto-super-enhanced
+ * It has been simplified to assume the Open/Closed sensor is located physically
+ * on the same door as the Lock. Additionally, this has no need for "Unlock"
+ * so those features have been completely removed.
+ */
+
+preferences {
     page name: "mainPage", install: true, uninstall: true
 }
 
@@ -38,49 +47,53 @@ def mainPage() {
     }
 }
 
-def installed(){
+def installed() {
     initialize()
 }
 
-def updated(){
+def updated() {
     unsubscribe()
     unschedule()
     initialize()
 }
 
-def initialize(){
+def initialize() {
     log.debug "Settings: ${settings}"
     subscribe(lock1, "lock", doorHandler, [filterEvents: false])
     subscribe(contact, "contact.open", doorHandler)
     subscribe(contact, "contact.closed", doorHandler)
 }
 
-def lockDoor(){
+def lockDoor() {
     log.debug "Locking the door."
     lock1.lock()
     if(location.contactBookEnabled) {
-        if ( recipients ) {
-            log.debug ( "Sending Push Notification..." ) 
-            sendNotificationToContacts( "${lock1} locked after ${contact} was closed for ${secondsLater} seconds!", recipients)
+        if (recipients) {
+            log.debug ("Sending Push Notification...") 
+            sendNotificationToContacts("${lock1} locked after ${contact} was closed for ${secondsLater} seconds!", recipients)
         }
     }
     if (phoneNumber) {
         log.debug("Sending text message...")
-        sendSms( phoneNumber, "${lock1} locked after ${contact} was closed for ${secondsLater} seconds!")
+        sendSms(phoneNumber, "${lock1} locked after ${contact} was closed for ${secondsLater} seconds!")
     }
 }
 
-def doorHandler(evt){
-    if ((contact.latestValue("contact") == "closed") && (evt.value == "locked")) { // If the door is closed and a person manually locks it then...
-        unschedule( lockDoor ) // ...we don't need to lock it later.
+def doorHandler(evt) {
+    if (evt.value == "open") {
+        // Door was just opened. No need to lock it (until it closes).
+        unschedule(lockDoor)
     }   
-    else if ((contact.latestValue("contact") == "closed") && (evt.value == "unlocked")) { // If the door is closed and a person unlocks it then...
-        runIn( (secondsLater), lockDoor ) // ...schedule (in seconds) to lock.
+    else if ((evt.value == "locked") && (contact.latestValue("contact") == "closed")) {
+        // Lock was just locked and door is closed. No need to lock it.
+        unschedule(lockDoor)
+    }   
+    else if ((evt.value == "unlocked") && (contact.latestValue("contact") == "closed")) {
+        // Lock was just unlocked and door is closed. Lock it in secondsLater seconds.
+        runIn(secondsLater), lockDoor)
     }
-    else if ((lock1.latestValue("lock") == "unlocked") && (evt.value == "open")) { // If a person opens an unlocked door...
-        unschedule( lockDoor ) // ...we don't need to lock it later.
-    }
-    else if ((lock1.latestValue("lock") == "unlocked") && (evt.value == "closed")) { // If a person closes an unlocked door...
-        runIn( (secondsLater), lockDoor ) // ...schedule (in seconds) to lock.
+    else if ((evt.value == "closed") && (lock1.latestValue("lock") == "unlocked")) { // If a person closes an unlocked door...
+        // Door was just closed but lock is unlocked. Lock it in secondsLater seconds.
+        runIn(secondsLater, lockDoor)
     }
 }
